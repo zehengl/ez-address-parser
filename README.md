8ee2f14c8ace27669dc72498a6947c996a7fca6b
Zeheng Li <imzehengl@gmail.com>
2020-01-07 12:29:27 -0700

    initial commit from gh:zehengl/cookiecutter-py-package

---
+++ README.md
@@ -0,0 +1,9 @@
+# ez-address-parser
+
+![coding_style](https://img.shields.io/badge/code%20style-black-000000.svg)
+
+## Install
+
+## Test
+
+    python setup.py test
---
+++ setup.py
@@ -0,0 +1,28 @@
+from os import path
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name="ez-address-parser",
+    packages=find_packages(),
+    entry_points={"console_scripts": []},
+    include_package_data=True,
+    package_data={},
+    install_requires=[],
+    setup_requires=["setuptools_scm", "pytest-runner"],
+    use_scm_version=True,
+    tests_require=["pytest"],
+    test_suite="tests",
+    author="Zeheng li",
+    author_email="imzehengl@gmail.com",
+    maintainer="Zeheng li",
+    maintainer_email="imzehengl@gmail.com",
+    description="An address parser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zehengl/ez-address-parser",
+)
