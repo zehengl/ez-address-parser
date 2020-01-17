55343805c15fd76446a4d32174d73ffbc39e4521
Zeheng Li <imzehengl@gmail.com>
2020-01-17 14:14:42 -0700

    finish feature script

--- ez_address_parser/features.py
+++ ez_address_parser/features.py
@@ -1,5 +1,6 @@
 from string import digits
 from .constants import direction_terms, street_types, unit_designators
+from .utils import tokenize
 
 
 def is_direction_term(token):
@@ -23,11 +24,59 @@ def relates_to_digit(token):
         return "none"
 
 
+def relates_to_length(token):
+    if token.isdigit():
+        return f"d:{len(token)}"
+    else:
+        return f"w:{len(token)}"
+
+
 def token_to_feature(token, name):
     return {
         f"{name}-is-directioin-term": is_direction_term(token),
         f"{name}-is-street-type": is_street_type(token),
         f"{name}-is-unit-designator": is_unit_designator(token),
         f"{name}-is-title": token.istitle(),
+        f"{name}-is-alpha": token.isalpha(),
+        f"{name}-token": False if token.isdigit() else token.lower(),
         f"{name}-relate-to-digit": relates_to_digit(token),
+        f"{name}-relate-to-length": relates_to_length(token),
     }
+
+
+def tokens_to_instance(tokens):
+    instance = []
+    for i, token in enumerate(tokens):
+        feature = token_to_feature(token, "curr")
+        if i > 0:
+            token_prev = tokens[i - 1]
+            feature.update(token_to_feature(token_prev, "prev"))
+        else:
+            feature.update({"begin-of-address": True})
+
+        if i < len(tokens) - 1:
+            token_next = tokens[i + 1]
+            feature.update(token_to_feature(token_next, "next"))
+        else:
+            feature.update({"end-of-address": True})
+
+        instance.append(feature)
+
+    return instance
+
+
+def transform(address):
+    return tokens_to_instance(tokenize(address))
+
+
+def load_data(data):
+    X, y = [], []
+    for address in data:
+        tokens, labels = [], []
+        for token, label in address:
+            tokens.append(token)
+            labels.append(label)
+        X.append(tokens_to_instance(tokens))
+        y.append(labels)
+
+    return X, y
