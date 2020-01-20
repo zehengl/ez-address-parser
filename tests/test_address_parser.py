10b6eac9a025d41d9e55f1fd72552a4cb1a04d01
Zeheng Li <imzehengl@gmail.com>
2020-01-20 10:36:48 -0700

    complete tests

--- tests/test_address_parser.py
+++ tests/test_address_parser.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch, Mock
+
 import pytest
 
 from ez_address_parser import AddressParser
@@ -25,3 +27,33 @@ def test_AddressParser_with_pretrained_model():
         ("S", "StreetDirection"),
         ("W", "StreetDirection"),
     ]
+
+
+@patch("ez_address_parser.address_parser.load_data")
+@patch("ez_address_parser.address_parser.sklearn_crfsuite")
+def test_AddressParser_train(sklearn_crfsuite, load_data):
+    with pytest.warns(UserWarning) as warnings:
+        ap = AddressParser(use_pretrained=False)
+
+    c1 = 0
+    c2 = 0
+    data = []
+    X, y = [], []
+    crf = Mock()
+
+    load_data.return_value = (X, y)
+    sklearn_crfsuite.CRF.return_value = crf
+
+    ap.train(data, c1=c1, c2=c2)
+
+    load_data.assert_called_once_with(data)
+
+    sklearn_crfsuite.CRF.assert_called_once_with(
+        algorithm="lbfgs",
+        max_iterations=100,
+        all_possible_transitions=True,
+        c1=c1,
+        c2=c2,
+    )
+
+    crf.fit.assert_called_once_with(X, y)
